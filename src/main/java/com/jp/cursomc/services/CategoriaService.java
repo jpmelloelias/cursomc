545a170bf7924c103742f966a308e3397130ebc5
Joao Paulo <jpmelloelias@gmail.com>
2019-08-15 09:11:06 -0300

    Ajsutes no end point /categoria/{id}

--- src/main/java/com/jp/cursomc/services/CategoriaService.java
+++ src/main/java/com/jp/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.jp.cursomc.domain.Categoria;
 import com.jp.cursomc.repositories.CategoriaRepository;
+import com.jp.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,10 @@ public class CategoriaService {
 	
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+		//return obj.orElse(null);
 	}
 	
 	
