2de3137992c269152a5cad50d7bdb7bc62c567a7
Joao Paulo <jpmelloelias@gmail.com>
2019-12-11 09:47:28 -0200

    Adicionando senha Cliente

--- src/main/java/com/jp/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/jp/cursomc/dto/ClienteNewDTO.java
@@ -43,6 +43,11 @@ public class ClienteNewDTO implements Serializable {
 	private String telefone3;
 
 	private Integer cidadeId;
+	
+	
+	@NotEmpty(message = "Preenchimento obrigatório")
+	private String senha;
+
 
 	public ClienteNewDTO() {
 	}
@@ -150,4 +155,13 @@ public class ClienteNewDTO implements Serializable {
 	public void setCidadeId(Integer cidadeId) {
 		this.cidadeId = cidadeId;
 	}	
+	
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
 } 
